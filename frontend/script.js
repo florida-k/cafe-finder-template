4dab39db2018c4ea4eabaeb341fa4af62a005621
florida <floridaflk@gmail.com>
2026-03-25 19:03:57 -0400

    update render url

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 // ===== Configuration =====
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://cafe-finder-template-2zgm.onrender.com/";
 
 // ===== DOM Elements =====
 const findBtn = document.getElementById("find-btn");
